8e46b529772d233e1ea56c82acdea6748673f317
howard <bhowiebkr@gmail.com>
2019-12-22 16:24:06 -0500

    download audio and video to download folder.

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-import sys
-import os
 import youtube_dl
+import os
+
+DOWNLOAD_TYPE = "audio"  # audio / video
+URL = "https://www.youtube.com/watch?v=zJGvJtE94tY"
+
+
+def get_download_path():
+    """Returns the default downloads path for linux or windows"""
+    if os.name == "nt":
+        import winreg
+
+        sub_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+        downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser("~"), "downloads")
 
 
 def main():
 
-    music_folder = os.path.join(os.getenv("HOME"), "Music")
-    if not os.path.exists(music_folder):
-        os.makedirs(music_folder)
-
-    URL = sys.argv[1]
-
     params = {
         "format": "bestaudio/best",
-        "outtmpl": music_folder + "/%(uploader)s/%(title)s.%(ext)s",
+        "outtmpl": get_download_path() + os.sep + "%(title)s.%(ext)s",
         "ignoreerrors": True,
     }
 
+    if DOWNLOAD_TYPE == "video":
+        params["format"] = "bestvideo+bestaudio"
+
     with youtube_dl.YoutubeDL(params=params) as ydl:
-        ydl.extract_info(URL, download=True)
+        data = ydl.extract_info(URL, download=True)
 
 
 if __name__ == "__main__":
